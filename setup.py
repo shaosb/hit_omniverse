41911fae912acb8201c9a59dcb3ce0c23c343829
shaosb <shaosb97@gmail.com>
2024-08-09 13:31:30 +0800

    submit setup.py
    ssb 8.9

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='hit_omniverse',
+    version='0.4',
+    author='SSB',
+    author_email='shaosb97@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        "pyyaml",
+        "torch",
+        "gymnasium",
+        "numpy",
+    ],
+)
